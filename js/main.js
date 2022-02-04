25977e29595ba364ad7fe5878e25f05fd44a3216
Anmol Kumar <anmol4979199@gmail.com>
2022-02-05 00:38:44 +0530

    Showcase

---
+++ js/main.js
@@ -0,0 +1,28 @@
+
+/*a = "Anmol";
+b = 5;
+ x = a.split("");
+ scooter = {
+   name :"Lamreta", class:"3rd", useful: "True"
+ }
+ scooter.name = "Anmol"
+window.alert(scooter.name)
+
+//This is accessing Array
+
+const a = [
+  "Anmol", "Kumar", "An", "intelligent"
+]
+let  len = a.length;
+let list = "<table>";
+for (let i = 0; i<len; i++){
+  list += "<tr><td>" + a[i] + "</td></tr>";
+}
+  list += "</table>";
+document.getElementById("para").innerHTML = list;
+*/
+const a = [
+  "Anmol", "Kumar", "An", "intelligent"
+]
+text = a.toString();
+document.getElementById("para").innerHTML = text;
